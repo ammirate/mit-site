094a6a3cbd676a97202cf19b4b6c45c6f0265d1c
Antonio <cesarano2607@gmail.com>
2014-12-09 18:21:54 +0100

    Aggiunti bean Account e Person, con allegati Manager a classi di test in JUnit

---
+++ src/java/it/unisa/offerta_formativa/beans/Account.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.offerta_formativa.beans;
+
+/**
+ *
+ * @author tony
+ */
+public class Account {
+    
+    private String email;
+    
+    private String password;
+    
+    private String typeOfAccount;
+    
+    private boolean active;
+
+    public Account(String email, String password, String typeOfAccount) {
+        this.email = email;
+        this.password = password;
+        this.typeOfAccount = typeOfAccount;
+        this.active = true;
+    }
+    
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTypeOfAccount() {
+        return typeOfAccount;
+    }
+
+    public void setTypeOfAccount(String typeOfAccount) {
+        this.typeOfAccount = typeOfAccount;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    
+    
+    
+}
---
+++ src/java/it/unisa/offerta_formativa/manager/AccountManager.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.offerta_formativa.manager;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import it.unisa.offerta_formativa.beans.Account;
+
+/**
+ *
+ * @author Antonio
+ */
+public class AccountManager {
+
+    private Connection conn = null;
+    private Statement stmt;
+    public static String TABLE = "account";
+    public static String PKEY = "email";
+    private ResultSet rs = null;
+
+    private static AccountManager instance = null;
+
+    
+    private AccountManager() {}
+    /**
+     * Singleton pattern
+     *
+     * @return
+     */
+    public static AccountManager getInstance() {
+        if (instance == null) {
+            instance = new AccountManager();
+        }
+        return instance;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<Account> getAllAccounts() {
+        stmt = DBConnector.openConnection();
+
+        List<Account> toReturn = new ArrayList<>();
+        try {
+            String query = "SELECT * FROM " + TABLE ;
+            rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                Account a = getAccountFromResultSet(rs);
+                toReturn.add(a);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DBConnector.closeConnection();
+        }
+        return toReturn;
+    }
+    
+    
+
+    /**
+     *
+     * @param rs
+     * @return
+     */
+    private Account getAccountFromResultSet(ResultSet rs) {
+        String email;
+        try {
+            email = rs.getString("email");
+            String password = rs.getString("password");
+            String typeAccount = rs.getString("typeOfAccount");
+            return new Account(email, password, typeAccount);
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+}
